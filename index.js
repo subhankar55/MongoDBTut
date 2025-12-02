394e4070e0d31e8a5f8208875316f1051d0beb24
Subhankar <maitisubhankar55@gmail.com>
2025-12-02 16:01:06 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 // getting-started.js
 const mongoose = require('mongoose');
 
-main().catch(err => console.log(err));
-
 async function main() {
+  // Connect to DB
   await mongoose.connect('mongodb://127.0.0.1:27017/test');
 
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-} 
+  // Schema
+  const kittyschema = new mongoose.Schema({ name: String });
+
+  // Method
+  kittyschema.methods.speak = function speak() {
+    const greet = this.name
+      ? "Meow! name is: " + this.name
+      : "Name not available";
+    console.log(greet);
+  };
+
+  // Model
+  const Kitten = mongoose.model('kitten', kittyschema);
+
+  // Documents
+  const silent = new Kitten({ name: 'Silent' });
+  const fluffy = new Kitten({ name: 'Fluffy' });
+
+  // Use method
+  fluffy.speak();
+
+  // Save to DB
+  await silent.save();
+  await fluffy.save();
+
+  // Fetch from DB
+  const kittens = await Kitten.find();
+  console.log(kittens);
+}
+
+// Run main
+main().catch(err => console.log(err));
